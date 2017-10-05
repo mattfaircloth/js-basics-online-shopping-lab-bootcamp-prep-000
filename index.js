004cfa416f54f38c03d6de509e617f06ad792035
mattfaircloth <mattcfaircloth@gmail.com>
2017-10-05 17:17:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,5 +72,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber.length === 0) {
+    console.log(`Sorry, we don't have a credit card on file for you.`)
+  }
 }
