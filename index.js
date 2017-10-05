d05df0846b6ecdc5118149b030cc2d1c8b71325b
mattfaircloth <mattcfaircloth@gmail.com>
2017-10-05 17:29:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,4 +77,5 @@ function placeOrder(cardNumber) {
   } else {
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   }
+  return cart.slice(0, cart.length -1)
 }
